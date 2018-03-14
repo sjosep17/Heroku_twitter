df01bd56c360a9c4299e801ebba9a9e0d505988c
IFE <sundayjoseph@IFEs-MacBook-Pro.local>
2018-03-13 23:44:28 -0500

    my project

--- Chatterbot.py
+++ Chatterbot.py
@@ -1,1 +1,39 @@
-print("this is work")
+
+# Dependencies
+import tweepy
+import json
+import time
+
+# Twitter API Keys
+consumer_key = "FLQnH739anOsrDjzQjGb96RS3"
+consumer_secret = "jdbqRx4BbdsPhzWYsXHSLSjOq7snKKZ0yZtDfUYC8WnqmCB0Fl"
+access_token = "298358375-qK1nWXlDjGNMnvt4UnsqQd5Uy4W9s7BQDYSdET7Q"
+access_token_secret = "hPqw87aSWyMauJqb2vMAUNnJlcXtX99cfKUxyACI9eUav"
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+
+# Create a function that tweets
+def TweetOut(tweet_number):
+    api.update_status(
+        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        tweet_number)
+
+
+# Create a function that calls the TweetOut function every minute
+counter = 1
+
+# Infinitely loop
+while(True):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
